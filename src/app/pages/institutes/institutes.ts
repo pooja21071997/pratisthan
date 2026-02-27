4438d0571b3c704dd664d30f7e77ae5ec2b9eef8
poojaPersonal <100690066+pooja21071997@users.noreply.github.com>
2026-02-27 12:17:19 +0530

    build issue resolved

--- src/app/pages/institutes/institutes.ts
+++ src/app/pages/institutes/institutes.ts
@@ -13,44 +13,44 @@ export class Institutes {
     {
       name: 'Dr Avinash Achaarya Vidyalaya',
       image: 'assets/images/Institutes/Acharya.jpg',
-      link: 'http://localhost:4200/',
+      link: 'https://acharyavp-site-guei.vercel.app/',
       description:
         'Building Foundations with Semi English Pre-Primary And Primary Education',
     },
     {
       name: 'Primary And Secondary School',
       image: 'assets/images/Institutes/primarySecondary.png',
-      link: 'http://localhost:59367/',
+      link: 'https://primary-secondary-site.vercel.app/',
       description: 'Innovative Learning for Young Minds\nKG to Class 10',
     },
     {
       name: 'English Medium School',
       image: 'assets/images/Institutes/English.png',
-      link: 'http://localhost:59540/',
+      link: 'https://english-medium-site.vercel.app/',
       description: 'Empowering Minds Through Quality English Education\nKG to Class 10',
     },
     {
       name: 'Kashinath Palod Public School',
       image: 'assets/images/Institutes/kpps.png',
-      link: 'http://localhost:59697/',
+      link: 'https://kpps-site.vercel.app/',
       description: 'Imparting Excellence in CBSE Learning\nNursery to Class 10',
     },
     {
       name: 'Late Smt. B.G. Shanbhag Highschool',
       image: 'assets/images/Institutes/shanbhag.png',
-      link: 'http://localhost:59847/',
+      link: 'https://bgs-site-smoky.vercel.app/',
       description: 'Imparting Excellence in CBSE Learning\nNursery to Class 10',
     },
     {
       name: 'Shravan Vikas Mandir',
       image: 'assets/images/Institutes/Shravan.png',
-      link: 'http://localhost:60049/',
+      link: 'https://svm-site.vercel.app/',
       description: 'Empowering Diverse Learners through Specialized Education',
     },
     {
       name: 'Junior & Senior College',
       image: 'assets/images/Institutes/Clg.png',
-      link: 'http://localhost:60208/',
+      link: 'https://jrclg-site.vercel.app/',
       description:
         'Fostering Enlightenment Through Advanced Educational Pursuits',
       center: true,
